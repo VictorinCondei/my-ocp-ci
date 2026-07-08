94655b07df98b313776d2d992941961950d9c3ae
Victorin Condei <victorin.condei@ro.ibm.com>
2026-07-08 11:46:35 +0300

    upgrade features Gitleaks

--- vars/stageGitleaks.groovy
+++ vars/stageGitleaks.groovy
@@ -11,7 +11,7 @@ def call(Map config = [:]) {
 
     stage('Security — Gitleaks') {
 
-        sh "[ -e ${reportDir} ] || mkdir -p ${reportDir}"
+        sh "echo ${WORKSPACE}; [ -e ${reportDir} ] || mkdir -p ${reportDir}"
 
         int exitCode = sh(
             returnStatus: true,
